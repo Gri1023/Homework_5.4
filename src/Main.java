81accaddf7967dc3422a7b9bddcbeffe4cf3e2de
Gri1023 <hryhorii.bavykin@gmail.com>
2022-09-13 18:36:13 +0300

    improvement

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ public class Main {
         System.out.println(reverse(a));
         System.out.println(equal(a,b));
     }
-    static String reverse (int[] a) {
+    static int[] reverse (int[] a) {
         int reversed[] = new int [a.length];
         for (int i = a.length-1; i >= 0; i--) {
             System.out.print(a[i] + " ");
             reversed[i] = a[i];
         }
-        return null;
+        return reversed;
     }
     static boolean equal (int[] a, int[] b) {
         for (int i=0;i<a.length;i++)
